5a2acd5be6c4a28aadcd57e31fb12a43e861db3e
H Rodrigues <38510364+hryds@users.noreply.github.com>
2025-02-10 14:29:24 -0300

    Ligação entre nota fiscal e produção

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const verifyJWT = require('../middlewares/verifyJWT')
+
 const multer = require('multer');
 const { uploadFile } = require('../controllers/fileUpload');
 
@@ -7,12 +9,17 @@ const storage = multer.diskStorage({
         cb(null, './uploads');
     },
     filename: function (req, file, cb) {
-        cb(null, + Date.now() + '-' + file.originalname);
+        const producaoId = req.body.producaoId || 'unknown';
+        console.log(req.body.producaoId);
+        cb(null, `${producaoId}-${Date.now()}-${file.originalname}`);
     }
 });
 
 const upload = multer({ storage });
 
-router.post('/', upload.single("file"), uploadFile);
+router.post('/', verifyJWT, upload.single("file"), (req, res, next) => {
+    console.log(req.body.producaoId);
+    next();
+}, uploadFile);
 
 module.exports = router;
